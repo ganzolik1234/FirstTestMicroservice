6926633f7d31300bb123c67084f5122f0cf4da6d
VYACHESLAV <Vyacheslav.Dyumaev@paragon-software.com>
2020-05-22 14:17:31 +0300

    excluded security and fixed swagger

--- src/main/java/com/first/test/microservice/configuration/SwaggerConfiguration.java
+++ src/main/java/com/first/test/microservice/configuration/SwaggerConfiguration.java
@@ -2,9 +2,11 @@ package com.first.test.microservice.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
@@ -15,13 +17,14 @@ import java.util.Locale;
 
 @Configuration
 @EnableSwagger2WebMvc
+@Import(SpringDataRestConfiguration.class)
 public class SwaggerConfiguration {
 
     @Bean
     public Docket exportServiceApi() {
         return new Docket(DocumentationType.SWAGGER_2).select()
                 .apis(RequestHandlerSelectors.basePackage(
-                        "de.pearl.microservices.controller"))
+                        "com.first.test.microservice.controller"))
                 .paths(PathSelectors.any()).build().pathMapping("/")
                 .protocols(new HashSet<>(Collections.singletonList("http")))
                 .directModelSubstitute(LocalDate.class, String.class)
